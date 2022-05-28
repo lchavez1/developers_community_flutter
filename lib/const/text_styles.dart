8eb38c1b22376aebbb1e8d279e51cfaa0870be95
tomvargas <tomvargasd@gmail.com>
2022-05-28 18:13:21 -0500

    optimize SplashScreen using gradient colors, animations and icons

--- lib/const/text_styles.dart
+++ lib/const/text_styles.dart
@@ -6,12 +6,18 @@ const title_diamond_notation = TextStyle(
     fontStyle: FontStyle.normal,
     fontSize: 36,
     fontWeight: FontWeight.bold,
-    color: gray_color
-);
+    color: gray_color);
 
 const title_text = TextStyle(
   fontStyle: FontStyle.normal,
   fontSize: 36,
   fontWeight: FontWeight.bold,
   color: Color.fromRGBO(0, 132, 255, 1),
+);
+
+const splashBtnTextStyle = TextStyle(
+  fontStyle: FontStyle.normal,
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
 );
